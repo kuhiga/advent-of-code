e9090928770cf9c9930856155351dda8aeed88b1
Kurt Higa <kurthigaa@gmail.com>
2024-12-02 19:14:53 -0800

    day1 complete

--- day-1/a.ts
+++ day-1/a.ts
@@ -1,9 +1,22 @@
-import { runSolution } from '../utils.ts';
-
+import { runSolution } from "../utils.ts";
+// https://adventofcode.com/2024/day/1
 /** provide your solution as the return of this function */
 export async function day1a(data: string[]) {
-  console.log(data);
-  return 0;
+  const list1: number[] = [];
+  const list2: number[] = [];
+  //separate into two lists
+  for (let i = 0; i < data.length; i++) {
+    list1.push(parseInt(data[i].split("   ")[0]));
+    list2.push(parseInt(data[i].split("   ")[1]));
+  }
+  list1.sort();
+  list2.sort();
+  let sum = 0;
+  for (let i = 0; i < data.length; i++) {
+      sum += Math.abs(list1[i] - list2[i])
+  }
+
+  return sum;
 }
 
 await runSolution(day1a);
--- day-1/b.ts
+++ day-1/b.ts
@@ -1,9 +1,27 @@
-import { runSolution } from '../utils.ts';
+import { runSolution } from "../utils.ts";
 
 /** provide your solution as the return of this function */
+// Calculate a total similarity score by adding up each number in the left list
+// after multiplying it by the number of times that number appears in the right list.
 export async function day1b(data: string[]) {
-  console.log(data);
-  return 0;
+  const list1: number[] = [];
+  const list2: number[] = [];
+  const myMap = new Map();
+  //separate into two lists
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].trim() === "") continue;
+    const [first, second] = data[i].split(/\s+/).map(Number);
+    list1.push(first);
+    list2.push(second);
+    myMap.set(second, (myMap.get(second) || 0) + 1);
+  }
+  list1.sort();
+  list2.sort();
+  let sum = 0;
+  for(let i = 0; i < list1.length; i++){
+    sum += list1[i] * (myMap.get(list1[i]) ?? 0);
+  }
+  return sum;
 }
 
 await runSolution(day1b);
