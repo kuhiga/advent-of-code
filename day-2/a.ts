dd36e099ce934a571e3950ef3a71a776667cc124
Kurt Higa <kurthigaa@gmail.com>
2024-12-03 22:47:33 -0800

    finish day 2 part 1

--- day-2/a.ts
+++ day-2/a.ts
@@ -1,9 +1,26 @@
-import { runSolution } from '../utils.ts';
+import { runSolution } from "../utils.ts";
+
+// The levels are either all increasing or all decreasing.
+// Any two adjacent levels differ by at least one and at most three.
 
 /** provide your solution as the return of this function */
 export async function day2a(data: string[]) {
-  console.log(data);
-  return 0;
+  const reports = [];
+  for (let i = 0; i < data.length; i++) {
+    reports.push((data[i].split(" ")).map(Number));
+  }
+  return reports.filter(isSafe).length;
+}
+
+function isSafe(levels: number[]){
+  const direction = Math.sign(levels[1] - levels[0]);
+  for(let i = 0, j = 1; j < levels.length; i++, j++ ){
+    const difference = levels[j] - levels[i];
+    if(Math.sign(difference) !== direction || Math.abs(difference) > 3){
+      return false;
+    }
+  }
+  return true;
 }
 
 await runSolution(day2a);
