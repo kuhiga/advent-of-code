d38e6fbf73c075394013c82c01e74e41a53cd76b
Kurt Higa <kurthigaa@gmail.com>
2024-12-04 20:07:22 -0800

    finish day 2 part b

--- day-2/b.ts
+++ day-2/b.ts
@@ -1,9 +1,44 @@
-import { runSolution } from '../utils.ts';
+import { runSolution } from "../utils.ts";
 
+// The levels are either all increasing or all decreasing.
+// Any two adjacent levels differ by at least one and at most three.
+// safe if able to remove levels
 /** provide your solution as the return of this function */
-export async function day2b(data: string[]) {
-  console.log(data);
-  return 0;
+export async function day2a(data: string[]) {
+  const reports = [];
+  for (let i = 0; i < data.length; i++) {
+    reports.push(data[i].split(" ").map(Number));
+  }
+  return reports.filter(isSafe).length;
 }
 
-await runSolution(day2b);
+function isSafe(levels: number[]) {
+  if (isSafeWithoutRemoving(levels)) {
+    return true;
+  } else {
+    for (let i = 0; i < levels.length; i++) {
+      if (isSafeWithoutRemoving(levels.toSpliced(i, 1))) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
+
+function isSafeWithoutRemoving(levels: number[]) {
+  const direction = Math.sign(levels[1] - levels[0]);
+  if (direction === 0) return false;
+  for (let i = 0, j = 1; j < levels.length; i++, j++) {
+    const difference = levels[j] - levels[i];
+    if (
+      Math.sign(difference) !== direction ||
+      Math.abs(difference) > 3 ||
+      Math.abs(difference) < 1
+    ) {
+      return false;
+    }
+  }
+  return true;
+}
+
+await runSolution(day2a);
