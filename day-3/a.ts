76316e9b00bba3268a84b3f8c57f18cd98c6e59d
Kurt Higa <kurthigaa@gmail.com>
2024-12-26 23:58:18 -0800

    day3a

--- day-3/a.ts
+++ day-3/a.ts
@@ -1,9 +1,16 @@
-import { runSolution } from '../utils.ts';
+import { runSolution } from "../utils.ts";
 
 /** provide your solution as the return of this function */
 export async function day3a(data: string[]) {
-  console.log(data);
-  return 0;
+  let sum = 0;
+  for (let i = 0; i < data.length; i++) {
+    for (const { groups } of data[i].matchAll(/mul\((?<a>\d+),(?<b>\d+)\)/g)) {
+      const { a, b } = groups!;
+      sum += +a * +b;
+    }
+  }
+
+  return sum;
 }
 
 await runSolution(day3a);
